a97ada9bfec706458fe2c8e4b83f4cba9fee5897
srinadhkonduri <srinadhkonduri@gmail.com>
2024-10-10 13:08:37 +0530

    bubble sort

---
+++ lecture18_sorting/bubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+// creating a function for bubble sort
+void bubbleSort(int arr[], int n){
+    // some times the arr is already sorted in that case
+    // we can perform an optimization
+    bool isSwap = false;
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if(!isSwap){ // array is already sorted
+            return;
+        }
+    }
+}
+// creating a function for printing the array
+void printArray(int arr[],int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main(){
+    int arr[] = {4,2,3,1,5};
+    int n = 5;
+
+// calling bubble sort
+    bubbleSort(arr,n);
+    printArray(arr,n);
+    return 0;
+}
